cbd413917ef502aea36030247140159ec34bac29
HACKONDEX <sagatelyan.aa@phystech.edu>
2020-03-14 01:12:31 +0300

    aaaa

--- task2/C/main_test.cpp
+++ task2/C/main_test.cpp
@@ -2,9 +2,9 @@
 #include <iostream>
 
 TEST(CTest1, DemnostrateGTestMacros){
-    EXPECT_TRUE(true);
+    // EXPECT_TRUE(true);
 }
 
 TEST(CTest2, DemnostrateGTestMacros2){
-    EXPECT_EQ(true, true);
+    // EXPECT_EQ(true, true);
 }
