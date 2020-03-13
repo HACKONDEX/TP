1c1a9fef3a993f07290ccc46c6f6a5c6a3e59fa4
HACKONDEX <sagatelyan.aa@phystech.edu>
2020-03-13 22:50:58 +0300

    TP task2 duble 3

---
+++ task2/C/main_test.cpp
@@ -0,0 +1,9 @@
+#include "../googletest/googletest/include/gtest/gtest.h"
+
+TEST(CTest1, DemnostrateGTestMacros){
+    EXPECT_TRUE(true);
+}
+
+TEST(CTest2, DemnostrateGTestMacros2){
+    EXPECT_EQ(true, true);
+}
