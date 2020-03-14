75bc12955659137577ef6da5a8199597a8896647
HACKONDEX <sagatelyan.aa@phystech.edu>
2020-03-14 11:51:53 +0300

    3 points

--- task2/C/main_test.cpp
+++ task2/C/main_test.cpp
@@ -1,10 +1,10 @@
 #include "gtest/gtest.h"
 #include <iostream>
 
-TEST(CTest1, DemnostrateGTestMacros){
-    // EXPECT_TRUE(true);
-}
+// TEST(CTest1, DemnostrateGTestMacros){
+//     // EXPECT_TRUE(true);
+// }
 
-TEST(CTest2, DemnostrateGTestMacros2){
-    // EXPECT_EQ(true, true);
-}
+// TEST(CTest2, DemnostrateGTestMacros2){
+//     // EXPECT_EQ(true, true);
+// }
